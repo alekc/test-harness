426bfb1b046b599adb1442c44711c51e636b6982
Alexander Chernov <alexander.chernov@globallogic.com>
2025-11-25 13:19:41 +0000

    xyz

--- main.tf
+++ main.tf
@@ -8,18 +8,18 @@ terraform {
 }
 
 # Random pet name for a web server
-# resource "random_pet" "web_server" {
-#   length = 3
-#   separator = "-"
-# }
+resource "random_pet" "web_server" {
+  length = 3
+  separator = "-"
+}
 
 # Random pet name for a database
 resource "random_pet" "database" {
   length = 3
   separator = "_"
 }
-resource "random_pet" "foo" {
-  length = 3
-  separator = "_"
-}
+# resource "random_pet" "foo" {
+#   length = 3
+#   separator = "_"
+# }
 
